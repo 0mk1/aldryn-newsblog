ed4f1459f6349b5aab53f94ba6db24ff98f6edb5
Mateusz Kamycki <mateusz.kamycki@gmail.com>
2018-03-20 12:14:31 +0100

    Fix #482 - add support for new way of render plugin introduced in django-cms 3.4.0
    * Also in `get_plugin_index_data` change `instance` name to
    `plugin_instance` to improve function readability

--- aldryn_newsblog/utils/utilities.py
+++ aldryn_newsblog/utils/utilities.py
@@ -22,6 +22,11 @@ except ImportError:
 from django.utils.html import strip_tags as _strip_tags
 from django.utils.text import smart_split
 
+try:
+    # Only django-cms>=3.4
+    from cms.plugin_rendering import ContentRenderer
+except ImportError:
+    pass
 from cms.utils.i18n import force_language, get_language_object
 
 from lxml.html.clean import Cleaner as LxmlCleaner
@@ -109,20 +114,33 @@ def get_field_value(obj, name):
     return value
 
 
+def render_plugin(request, plugin_instance):
+    if getattr(plugin_instance, 'render_plugin', None):
+        # Backward compatibility for django-cms<3.4
+        return plugin_instance.render_plugin(
+            context=RequestContext(request),
+        )
+    else:
+        renderer = ContentRenderer(request)
+        context = RequestContext(request)
+        context['request'] = request
+        return renderer.render_plugin(plugin_instance, context)
+
+
 def get_plugin_index_data(base_plugin, request):
     text_bits = []
 
-    instance, plugin_type = base_plugin.get_plugin_instance()
+    plugin_instance, plugin_type = base_plugin.get_plugin_instance()
 
-    if instance is None:
+    if plugin_instance is None:
         # this is an empty plugin
         return text_bits
 
-    search_fields = getattr(instance, 'search_fields', [])
+    search_fields = getattr(plugin_instance, 'search_fields', [])
 
-    if hasattr(instance, 'search_fulltext'):
+    if hasattr(plugin_instance, 'search_fulltext'):
         # check if the plugin instance has search enabled
-        search_contents = instance.search_fulltext
+        search_contents = plugin_instance.search_fulltext
     elif hasattr(base_plugin, 'search_fulltext'):
         # now check in the base plugin instance (CMSPlugin)
         search_contents = base_plugin.search_fulltext
@@ -135,13 +153,11 @@ def get_plugin_index_data(base_plugin, request):
         search_contents = not bool(search_fields)
 
     if search_contents:
-        plugin_contents = instance.render_plugin(
-            context=RequestContext(request))
-
+        plugin_contents = render_plugin(request, plugin_instance)
         if plugin_contents:
             text_bits = get_cleaned_bits(plugin_contents)
     else:
-        values = (get_field_value(instance, field) for field in search_fields)
+        values = (get_field_value(plugin_instance, field) for field in search_fields)
 
         for value in values:
             cleaned_bits = get_cleaned_bits(value or '')
